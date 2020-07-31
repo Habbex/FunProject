3131db490f94db56efe053175dde9c51cb06e0d0
Habbex <ihabokal@hotmail.com>
2020-07-31 09:50:39 +0000

    working base solution+ gitignore

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -27,7 +27,7 @@ namespace backend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.addSwaggerGen();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,7 +39,7 @@ namespace backend
             }
                
             app.UseHttpsRedirection();
-
+            app.UseSwagger();
             app.UseSwaggerUI(c =>
                 {
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
